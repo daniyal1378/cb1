76e34ae0bd12fdbd18aca9197847fe0263e6fdba
daniyal1378 <42182140+daniyal1378@users.noreply.github.com>
2019-08-22 22:00:55 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = app.listen(4000);
+const server = app.listen(process.env.PORT || 8080);
 const socket = require('socket.io');
 const io = socket(server);
 
